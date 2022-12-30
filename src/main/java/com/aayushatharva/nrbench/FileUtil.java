e4dbd53f9830a8974a3b4d0a9b1ddd68a5d4c633
Aayush Atharva <aayush@shieldblaze.com>
2022-12-30 18:10:54 +0530

    Memory mapped file reads

--- src/main/java/com/aayushatharva/nrbench/FileUtil.java
+++ src/main/java/com/aayushatharva/nrbench/FileUtil.java
@@ -1,12 +1,24 @@
 package com.aayushatharva.nrbench;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class FileUtil {
 
     public static String readFileAsString(String path) throws IOException {
-        return Files.readString(Path.of(path));
+//        return Files.readString(Path.of(path));
+        try (RandomAccessFile file = new RandomAccessFile(path, "r")) {
+            FileChannel channel = file.getChannel();
+
+            // Map the file into memory
+            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+
+            return StandardCharsets.UTF_8.decode(buffer).toString();
+        }
     }
 }
