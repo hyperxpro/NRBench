c40f7b013d00e48d1048168eb79c0c82f85d7657
Aayush Atharva <aayush@shieldblaze.com>
2022-12-30 15:31:05 +0530

    Revert

--- src/main/java/com/aayushatharva/nrbench/FileUtil.java
+++ src/main/java/com/aayushatharva/nrbench/FileUtil.java
@@ -5,17 +5,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public final class FileUtil {
 
     public static String readFileAsString(String path) throws IOException {
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8))) {
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = in.readLine()) != null) {
-                sb.append(line);
-            }
-            return sb.toString();
-        }
+//        try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8))) {
+//            StringBuilder sb = new StringBuilder();
+//            String line;
+//            while ((line = in.readLine()) != null) {
+//                sb.append(line);
+//            }
+//            return sb.toString();
+//        }
+        return Files.readString(Path.of(path));
     }
 }
